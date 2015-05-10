a5f19e0040b41cc2430fd938c6a53456582642ba
José Cabo <folcwine@gmail.com>
2015-05-10 13:43:32 +0200

    Updates project description.

---
+++ README.md
@@ -0,0 +1,9 @@
+# bootstrap3-modals
+
+Bootstrap 3 modals properly integated within Meteor platform.
+
+### Installation
+
+```bash
+meteor add bloodsucker:bootstrap3-modals
+```
--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'bloodsucker:bootstrap-modal',
+  name: 'bloodsucker:bootstrap3-modals',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Bootstrap 3 modals properly integated within Meteor platform.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Bloodsucker/meteor-bootstrap3-modals.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
